e9acab618c83e3f872474dd3da214cb77afd920b
unknown <vetleb@stud.ntnu.no>
2018-08-25 14:36:31 +0200

    Create main file

---
+++ main.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+import math
+import matplotlib.pyplot as PLT
+#import tflowtools as TFT
+
+def main(dimensions, HAF, OAF, loss_function, learn_rate, IWR, optimizer, data_source, case_fraction,
+         validation_fraction, validation_interval, test_fraction, minibatch_size, map_batch_size, steps, layers,
+         map_dendrograms, display_weights, display_biases):
+    pass
+
+
+def main2():
+    pass
+
+
+main2()
