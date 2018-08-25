b8bee82a4c47638c990475681f8b96c1b8dafb6b
unknown <vetleb@stud.ntnu.no>
2018-08-25 15:06:15 +0200

    Add gitignore and mnist folder

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import math
 import matplotlib.pyplot as PLT
 #import tflowtools as TFT
 
+from mnist import mnist_basics
+
 def main(dimensions, HAF, OAF, loss_function, learn_rate, IWR, optimizer, data_source, case_fraction,
          validation_fraction, validation_interval, test_fraction, minibatch_size, map_batch_size, steps, layers,
          map_dendrograms, display_weights, display_biases):
@@ -11,7 +13,7 @@ def main(dimensions, HAF, OAF, loss_function, learn_rate, IWR, optimizer, data_s
 
 
 def main2():
-    pass
+    mnist_basics.load_all_flat_cases()
 
 
 main2()
