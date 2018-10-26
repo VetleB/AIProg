55c1039b65ba21a6a530d62d4b7ab9220d928534
VetleB <vetleb@stud.ntnu.no>
2018-10-26 10:36:36 +0200

    Add .rar to gitignore

--- Project2/main.py
+++ Project2/main.py
@@ -2,11 +2,11 @@ import play
 import nim
 
 def main():
-    game_kwargs = {'stones': 99, 'move_size': 6, 'verbose': False}
+    game_kwargs = {'stones': 39, 'move_size': 3, 'verbose': True}
     game = nim.Nim
     rollouts = 400
     player_start = 1
-    batch_size = 20
+    batch_size = 1
     verbose = False
 
     p = play.Play(game_kwargs, game, rollouts, player_start, batch_size)
