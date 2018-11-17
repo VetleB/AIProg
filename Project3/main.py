e864017c5138463a1214ae9bde05bb0df5eb4f47
VetleB <vetleb@stud.ntnu.no>
2018-11-17 16:34:25 +0100

    Stuff

--- Project3/main.py
+++ Project3/main.py
@@ -6,9 +6,9 @@ import anet
 def main():
     side_length = 4
     game = hex.Hex
-    rollouts = (10, 's')
-    player_start = 0
-    batch_size = 1
+    rollouts = (500, 'r') # r -> amount ; s -> seconds
+    player_start = -1
+    batch_size = 1000
     num_matches = 1000
     verbose = False
     game_kwargs = {'side_length': side_length, 'verbose': verbose}
@@ -16,19 +16,20 @@ def main():
     nn_model = None
     input_layer_size = 2*side_length**2+2
     output_size = side_length**2
+    anet_name = 'anet_' + str(side_length) + 'x' + str(side_length)
     anet_kwargs = {'layers': [input_layer_size, 120, 64, output_size]
                 , 'haf': 'tanh'
                 , 'oaf': 'tanh'
                 , 'loss': 'mean_squared_error'
                 , 'optimizer': 'SGD'
-                , 'model_name': 'anet_4x4'
+                , 'model_name': anet_name
                 , 'pre_train_epochs': 500}
     pre_train = True
     run_train = False
 
 
-    #p = play.Play(game_kwargs, game, rollouts, player_start, batch_size, anet_kwargs=anet_kwargs)
-    #p.play_game(run_train=run_train, pre_train=pre_train)
+    p = play.Play(game_kwargs, game, rollouts, player_start, batch_size, anet_kwargs=anet_kwargs)
+    p.play_game(run_train=run_train, pre_train=pre_train)
 
     #anet_player = p.anet
     anet_player = anet.Anet(**anet_kwargs)
